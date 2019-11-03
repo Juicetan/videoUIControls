41fba4032949b6727fb406dcfaa4d5cbf08462e2
Justin <me@justinlam.ca>
2019-11-03 03:04:09 -0500

    Update build workflow to inject compiled css into content script;

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var replace = require('gulp-replace');
 var flatten = require('gulp-flatten');
 var concat = require('gulp-concat');
 var minifyCSS = require('gulp-minify-css');
+var fs = require('fs');
 
 gulp.task('clean', function(){
   return del([
@@ -14,7 +15,7 @@ gulp.task('clean', function(){
   ]);
 });
 
-gulp.task('build', function(){
+gulp.task('buildCSS', function(){
   return gulp.src('./app/**/*.scss')
       .pipe(sass())
       .pipe(prefix("last 2 versions"))
@@ -23,6 +24,18 @@ gulp.task('build', function(){
       .pipe(concat('style.min.css'))
       .pipe(gulp.dest('./.tmp/app/base/css'));
 });
+
+gulp.task('buildContentScript', function(){
+  var styleStr = fs.readFileSync('./.tmp/app/base/css/style.min.css');
+
+  return gulp.src('./content.js')
+      .pipe(replace(/<!-- tid:(.+?) -->/i, styleStr))
+      .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('build', gulp.series('buildCSS','buildContentScript', function(done){
+  done();
+}));
 
 gulp.task('watchChanges',function(){
   gulp.watch('./app/**/*',gulp.series('build',function(done){
